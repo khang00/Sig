249cfa01af50615c4f1e86a5d07c9c28ceab22cb
khang00 <dlqkhang8@gmail.com>
2021-03-26 19:33:16 +0700

    change to node http server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express from "express";
+import http from "http";
 
-// rest of the code remains same
 const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+const server = http.createServer(app);
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+
+server.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
