db7900e399420ac6f8d47dc569a4156a5a95d963
khang00 <dlqkhang8@gmail.com>
2021-05-18 13:55:18 +0700

    init test

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,6 @@
 import http from "http";
-import https from "https";
 import express, { Express, Request, Response } from "express";
-import { ChatRoom } from "./websocket";
 import { Api } from "./api";
-import { Persistence } from "./persistence";
-import HttpProxy from "http-proxy";
-// @ts-ignore
-import HttpProxyRules from "http-proxy-rules";
-import * as fs from "fs";
-import Signaling from "./websocket";
-import Metrics from "./metrics";
-import cors from "cors"
 
 const CORS_OPTION = {
   origin: "http://localhost"
@@ -18,70 +8,26 @@ const CORS_OPTION = {
 
 export default class Server {
   app: Express;
-  api: Api;
   port: number;
   server: http.Server;
-  signaling: Signaling;
-  metrics: Metrics;
-  proxy: HttpProxy;
-  proxyRules: any;
+  api: Api;
 
-  constructor(port: number, database: Persistence<ChatRoom>, secure = false) {
+  constructor(port: number) {
     this.app = express();
-    this.app.use(cors(CORS_OPTION));
     this.api = new Api();
-    this.server = this.createServer(this.app, secure);
-    this.port = port;
-    this.signaling = new Signaling(this.server, database);
-    this.metrics = new Metrics(this.signaling);
-    this.proxy = HttpProxy.createServer();
-    this.proxyRules = new HttpProxyRules({
-      rules: {
-        "/api/*": "http://localhost:10000/api"
-      },
-      default: "http://localhost:8000"
-    });
-  }
-
-  createServer(app: Express, secure: boolean): http.Server {
-    if (secure) {
-      const privateKey = fs.readFileSync("secret/key_from.pem");
-      const certificate = fs.readFileSync("secret/cert_from.pem");
-      const credentials = { key: privateKey, cert: certificate };
-      return new https.Server(credentials, app);
-    } else {
-      return new http.Server(app);
-    }
+    this.server = new http.Server(this.app);
+    this.port = port
   }
 
   start(onStarted: () => void) {
-    this.api.onRouter((router) => {
-      this.app.use("/api", router);
-      this.addMetricsHandler();
-      this.addProxyHandler();
-      this.server.listen(this.port, onStarted);
+    this.app.use("/api", (req :Request, resp :Response) => {
+      resp.status(200).end("halo")
     });
+
+    onStarted()
   }
 
   stop(onClosed: () => void) {
     this.server.close(onClosed);
-  }
-
-  private addMetricsHandler() {
-    this.app.get("/metrics", async (req: Request, res: Response) => {
-      const metrics = await this.metrics.getMetrics();
-      const contentType = this.metrics.getContentType();
-      res.set("Content-Type", contentType).end(metrics);
-    });
-  }
-
-  private addProxyHandler() {
-    this.app.get("/*", async (req: Request, res: Response) => {
-      const target = this.proxyRules.match(req);
-      this.proxy.web(req, res, { target }, (err) => {
-        console.error(err);
-        res.end("Can not proxy request");
-      });
-    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import Server from "./app";
-import { MemoryDB } from "./persistence";
-import { ChatRoom } from "./websocket";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
-if (process.env.NODE === "prod") {
-  const signalingDB = new MemoryDB<ChatRoom>();
-  const server = new Server(port, signalingDB, true);
-  server.start(() =>
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-  );
-} else {
-  const signalingDB = new MemoryDB<ChatRoom>();
-  const server = new Server(port, signalingDB, false);
-  server.start(() =>
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
-  );
-}
+
+const server = new Server(port);
+server.start(() =>
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+);
